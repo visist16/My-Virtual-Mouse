ef6dec4af553cd5d8babf2758f1db11f696fbd8f
Visist Tallam <57991461+visist16@users.noreply.github.com>
2022-09-12 23:03:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import cv2
 import mediapipe as mp
 import pyautogui
 cap = cv2.VideoCaptur(0)
+hand_detector = mp.solutions.hands.Hands()
+drawing_utils = mp.solutions.drawing_utils
+screen_width, screen_height = pyautogui.size()
+index_y = 0
 while True:
   _, frame = cap.read()
   cv2.imshow('Virtual Mouse', frame)
